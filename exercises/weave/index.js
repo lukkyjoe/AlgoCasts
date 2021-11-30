8502ccfe5f3bc6b52f9675a8b3b388bac3b694b5
Joseph Luk <joseph.sc.luk@gmail.com>
2021-11-30 09:52:35 -0800

    Implement stack and weave

--- exercises/weave/index.js
+++ exercises/weave/index.js
@@ -24,6 +24,38 @@
 
 const Queue = require('./queue');
 
-function weave(sourceOne, sourceTwo) {}
+function weave(sourceOne, sourceTwo) {
+  // peek source 1
+  // peek source 2
+  const q = new Queue()
+  while (sourceOne.peek() || sourceTwo.peek()) {
+    if (sourceOne.peek()) {
+      q.add(sourceOne.remove())
+    }
+    if (sourceTwo.peek()) {
+      q.add(sourceTwo.remove())
+    }
+  }
+  return q;
+
+}
+
+/*
+function weave(sourceOne, sourceTwo) {
+  const q = new Queue();
+
+  while (sourceOne.peek() || sourceTwo.peek()) {
+    if (sourceOne.peek()) {
+      q.add(sourceOne.remove());
+    }
+
+    if (sourceTwo.peek()) {
+      q.add(sourceTwo.remove());
+    }
+  }
+
+  return q;
+}
+*/
 
 module.exports = weave;
